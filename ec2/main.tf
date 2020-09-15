d0c0ee0bec217dd48d6f7f7bd4e054558017cd07
jayavardhanpatil <jayawardhan.patil@gmail.com>
2020-09-14 18:09:18 -0700

    Added load balancer

--- ec2/main.tf
+++ ec2/main.tf
@@ -17,9 +17,9 @@ data "aws_subnet_ids" "default_subnets" {
 
 data "aws_ami" "aws_linux_2_latest" {
   most_recent = true
-  owners = ["amazon"]
+  owners      = ["amazon"]
   filter {
-    name = "name"
+    name   = "name"
     values = ["amzn2-ami-hvm-*"]
   }
 }
@@ -58,13 +58,54 @@ resource "aws_security_group" "http_server_sg" {
   }
 }
 
-resource "aws_instance" "http_server" {
-  ami                    =  data.aws_ami.aws_linux_2_latest.id
+resource "aws_security_group" "elb_sg" {
+  name   = "elb_sg"
+  vpc_id = aws_default_vpc.default.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_elb" "elb" {
+  name = "elb"
+  subnets = data.aws_subnet_ids.default_subnets.ids
+  security_groups = [aws_security_group.elb_sg.id]
+  instances = values(aws_instance.http_servers).*.id
+
+  listener {
+    instance_port = 80
+    instance_protocol = "http"
+    lb_port = 80
+    lb_protocol = "http"
+  }
+}
+
+resource "aws_instance" "http_servers" {
+  ami                    = data.aws_ami.aws_linux_2_latest.id
   key_name               = "default-ec2"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.http_server_sg.id]
   //subnet_id              = "subnet-04123f3a"
-  subnet_id = tolist(data.aws_subnet_ids.default_subnets.ids)[0]
+  //subnet_id = tolist(data.aws_subnet_ids.default_subnets.ids)[0]
+
+  for_each  = data.aws_subnet_ids.default_subnets.ids
+  subnet_id = each.value
+
+  tags = {
+    name : "http_servers_${each.value}"
+  }
+
   connection {
     type        = "ssh"
     host        = self.public_ip
